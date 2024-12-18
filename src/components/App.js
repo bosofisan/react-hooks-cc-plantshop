5c4790638d8c4d7b606fe808fcb0bb628b27f716
lulu osofisan <boluosofisan@gmail.com>
2024-12-18 14:44:41 -0500

    GET /plants

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import PlantList from "./PlantList";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState ([]);
+
+  useEffect (() => {
+  fetch("http://localhost:6001/plants")
+  .then((response) => response.json())
+  .then((data) => setPlants(data));
+}, []);
+
   return (
     <div className="app">
+      <h1>Plantsy</h1>
+      <PlantList plants={plants} />
       <Header />
       <PlantPage />
     </div>
